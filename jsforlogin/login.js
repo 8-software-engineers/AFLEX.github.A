c74ca0453e96827cf699042ac57c74ae65b1f982
8-software-engineers <103649338+8-software-engineers@users.noreply.github.com>
2022-05-01 14:51:23 +0200

    Update login.js

--- jsforlogin/login.js
+++ jsforlogin/login.js
@@ -3,7 +3,7 @@ document.getElementById("error").style.display = 'none';
 document.getElementById("action").onsubmit = function () {
     let username = document.getElementById("Username").value;
     let password = document.getElementById("Password").value;
-    if (username == 'aflex' && password == '8ahmedinoneteam') {
+    if ((username == 'aflex' && password == '8ahmedinoneteam')||(username == 'ahmed' && password == '123456789')||(username == 'Bishoy' && password == '20 deg for every one')) {
     return true;
 }
 else{
@@ -11,27 +11,4 @@ else{
 
 return false;
 }
-}
-// Create a function for setting a variable value
-function myFunction_set1() {
-    var rs = getComputedStyle(r);
-    // Set the value of variable --blue to another value (in this case "lightblue")
-    r.style.setProperty('--main-color', '#dca523');
-    r.style.setProperty('--main-color-more-dark2', '#a07512');
-    r.style.setProperty('--main-color-more-dark1 ', '#684d0f');
-    r.style.setProperty('--second-color', '#ffffff1a');
-    r.style.setProperty('--background-color', '#0f0a05');
-    r.style.setProperty('--second-text-color', '#fff');
-    r.style.setProperty('--important-red', '#b50a0a');
-}
-function myFunction_set2() {
-    var rs = getComputedStyle(r);
-    // Set the value of variable --blue to another value (in this case "lightblue")
-    r.style.setProperty('--main-color', '#FFFFFF');
-    r.style.setProperty('--main-color-more-dark2', '#333');
-    r.style.setProperty('--main-color-more-dark1', '#000000');
-    r.style.setProperty('--second-color', '#053933');
-    r.style.setProperty('--background-color', '#181D25');
-    r.style.setProperty('--second-text-color', '#333');
-    r.style.setProperty('--important-red', 'green');
 }
