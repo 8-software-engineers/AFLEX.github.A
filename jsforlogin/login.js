0b12c87b46f80d5c1808da9a2025c46648d31c68
8-software-engineers <103649338+8-software-engineers@users.noreply.github.com>
2022-05-03 22:17:56 +0200

    Update login.js

--- jsforlogin/login.js
+++ jsforlogin/login.js
@@ -2,7 +2,7 @@ document.getElementById("error").style.display = 'none';
 document.getElementById("action").onsubmit = function () {
     let username = document.getElementById("Username").value;
     let password = document.getElementById("Password").value;
-    if ((username == 'aflex' && password == '8ahmedinoneteam')||(username == 'ahmed' && password == '123456789')||(username == 'Bishoy' && password == '20 deg for every one')) {
+    if ((username == 'aflex' && password == '8ahmedinoneteam')||(username == 'ahmed' && password == '123456789')||(username == 'Bishoy' && password == '20 deg for every one') ||(username == localStorage.new_account_username && password == localStorage.new_account_password) ) {
         localStorage.setItem('name_profile',username);
     return true;
 }
