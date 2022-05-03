ffb68c9069cbe4b533ff122c1db79500ce03384e
8-software-engineers <103649338+8-software-engineers@users.noreply.github.com>
2022-05-03 22:12:12 +0200

    Gom3a
    creat -> reda

---
+++ creat js/creat.js
@@ -0,0 +1,4 @@
+let new_user_name = document.getElementById('login-username');
+let new_password = document.getElementById('login-username');
+localStorage.setItem('new_account_username',new_user_name.value);
+localStorage.setItem('new_account_password',new_password.value);
