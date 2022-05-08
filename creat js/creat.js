23a3ad1787affa6d690dc518cedb27d86f518c1f
8-software-engineers <103649338+8-software-engineers@users.noreply.github.com>
2022-05-08 10:54:43 +0200

    Update creat.js

--- creat js/creat.js
+++ creat js/creat.js
@@ -1,4 +1,8 @@
 let new_user_name = document.getElementById('login-username');
 let new_password = document.getElementById('login-username');
-localStorage.setItem('new_account_username',new_user_name.value);
-localStorage.setItem('new_account_password',new_password.value);
+new_user_name.onkeyup = function() {
+    localStorage.setItem('new_account_username',new_user_name.value);
+}
+new_password.onkeyup = function() {
+    localStorage.setItem('new_account_password',new_password.value);
+}
