a2ad799d76aa65346a076aea1b3f33ef19512b19
8-software-engineers <103649338+8-software-engineers@users.noreply.github.com>
2022-05-09 12:46:32 +0200

    Update header.js

--- js for header/header.js
+++ js for header/header.js
@@ -12,28 +12,3 @@ btnOpen.onclick=function(){
     btnClose.classList.remove('hide');
     btnOpen.classList.add('hide');
 }
-// Get the root element
-var r = document.querySelector(':root'); 
-// Create a function for setting a variable value
-function myFunction_set1() {
-    var rs = getComputedStyle(r);
-    // Set the value of variable --blue to another value (in this case "lightblue")
-    r.style.setProperty('--main-color', '#dca523');
-    r.style.setProperty('--main-color-more-dark2', '#a07512');
-    r.style.setProperty('--main-color-more-dark1', '#684d0f');
-    r.style.setProperty('--second-color', '#ffffff1a');
-    r.style.setProperty('--background-color', '#0f0a05');
-    r.style.setProperty('--second-text-color', '#fff');
-    r.style.setProperty('--important-red', '#b50a0a');
-}
-function myFunction_set2() {
-    var rs = getComputedStyle(r);
-    // Set the value of variable --blue to another value (in this case "lightblue")
-    r.style.setProperty('--main-color', '#FFFFFF');
-    r.style.setProperty('--main-color-more-dark2', '#FFFFFF');
-    r.style.setProperty('--main-color-more-dark1', '#FFFFFF');
-    r.style.setProperty('--second-color', '#FFFFFF');
-    r.style.setProperty('--background-color', '#181D25');
-    r.style.setProperty('--second-text-color', '#FFFFFF');
-    r.style.setProperty('--important-red', 'green');
-}
